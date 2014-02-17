19383cbf73a172ec60e099ba2e9c1a3165237ca2
Ben Brunton <ben@flashtalking.com>
2014-02-17 17:59:31 +0000

    add locations

--- python/Game.py
+++ python/Game.py
@@ -1,4 +1,5 @@
 from multiChoice import ask
+from Location import Location
 
 def x():
 	print 'yo'
@@ -7,10 +8,23 @@ options = [
 	{'code': '1', 'description':'hello', 'callback': x}
 ]
 
+
+
 class Game:
-	def __init__(self, name):
+	currentLocation = 0
+	def __init__(self, name, locations):
+		self.locations = getLocations(locations)
 		self.name = name;
 
 	def start(self):
 		print 'hi ' + self.name
-		ask('would you like?', options)
+		print 'you are at ' + self.locations[self.currentLocation].name
+		#ask('would you like?', options)
+
+
+
+def getLocations(locationNames):	
+	locationList = []
+	for location in locationNames:
+		locationList.append(Location(location))
+	return locationList
--- python/main.py
+++ python/main.py
@@ -1,12 +1,14 @@
 import os
 from Game import Game
 
+locationNames = ['Downtown', 'Uptown', 'Suburbs', 'Docks']
+
 
 def start():
 	os.system('clear');
 	print 'dope wars py'
 	name = raw_input('what\'s your name?\n')
-	game = Game(name)
+	game = Game(name, locationNames)
 	game.start()
 
 start()
